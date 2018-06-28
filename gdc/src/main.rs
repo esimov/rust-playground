f414653da93e5db6ffbe033dfee7a1c2603658bf
esimov <esimovaq@yahoo.com>
2018-06-28 17:39:05 +0300

    Command line arguments

--- gdc/src/main.rs
+++ gdc/src/main.rs
@@ -1,6 +1,20 @@
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    let res = gdc(120, 48);
-    println!("The greatest common devisor: {}", res)
+    let mut numbers: Vec<u64> = Vec::new();
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gdc(d, *m);
+    }
+    println!("The greatest common devisor of {:?} is {}", numbers, d);
 }
 
 fn gdc(mut n: u64, mut m: u64) -> u64 {
