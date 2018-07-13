73c11bf331504a11b247bdb8ada914911a3a5635
esimov <esimovaq@yahoo.com>
2018-07-13 18:17:16 +0300

    Mandelbrot example

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,52 @@
+#![allow(dead_code, unused_variables)]
+
+extern crate num;
+use num::Complex;
+
+fn main() {
+    escape_time(Complex{re: 2.1, im: 0.2}, 1);
+}
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex {re: 0.0, im: 0.0};
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
+}
+
+use std::str::FromStr;
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None,
+            }
+        },
+        None => None,
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10,",     ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex{re, im}),
+        None => None,
+    }
+}
