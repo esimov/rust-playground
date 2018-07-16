105a64134ec8a8193d30dfe421032edab586dc44
esimov <esimovaq@yahoo.com>
2018-07-16 18:01:36 +0300

    Adding test case

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -50,3 +50,9 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
         None => None,
     }
 }
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex{re:1.25, im: -0.0625}));
+    assert_eq!(parse_complex("-0.0625"), None);
+}
