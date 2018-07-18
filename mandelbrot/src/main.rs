2b166d56ae904d6b94d5d574f54c3c1d77ace39b
esimov <esimovaq@yahoo.com>
2018-07-18 18:50:13 +0300

    Plotting image

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -108,3 +108,22 @@ fn render(
         }
     }
 }
+
+extern crate image;
+use image::png::PNGEncoder;
+use image::ColorType;
+use std::fs::File;
+use std::io::Result;
+
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<()> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
+
+    Ok(())
+}
