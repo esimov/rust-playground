d1255a007c96f1fd7643b853c6a20f84230eca6c
esimov <esimovaq@yahoo.com>
2018-07-25 18:52:11 +0300

    Concurrent mandelbrot

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code, unused_variables)]
 
+extern crate crossbeam;
 extern crate num;
+
 use num::Complex;
 use std::io::Write;
 
@@ -21,13 +23,33 @@ fn main() {
 
         std::process::exit(1);
     }
+
     let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
     let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
     let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
 
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
 
